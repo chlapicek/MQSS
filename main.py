a2701c24110316d4d4ee1ccbceb08ed26ea25101
Vojtěch Straka <vojtstraka@gmail.com>
2023-12-06 23:46:30 +0100

    Removed unused code Added linalg for enough determined variables

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas
 import galois
 # import time
 import itertools
+import re
 # import numba
 import cProfile
 import pstats
@@ -13,9 +14,8 @@ from z3 import *
 from tqdm import tqdm
 
 class GF2Mat(pandas.DataFrame):
-    # solution = {}
-    # guessed_variables = []
     most_common: dict[str, int] = {}
+    vars_needed: int = -1
 
     def drop_zeros_only_columns(self) -> pandas.DataFrame:
         # https://stackoverflow.com/questions/21164910/how-do-i-delete-a-column-that-contains-only-zeros-in-pandas
@@ -82,250 +82,54 @@ class GF2Mat(pandas.DataFrame):
         return self.loc[~(self == 0).all(axis=1)]
 
 
-    # def get_degree_submatrix(self, degree: int) -> pandas.DataFrame:
-    #     column_names = list(self.columns)
-    #     column_names = list(filter(lambda x: len(x) == degree, column_names))
-    #     if (degree == 0):
-    #         column_names = [(-1,)]
-    #     elif (degree == 1):
-    #         column_names.pop()
-    #     return self.loc[:, self.columns.isin(column_names)]
+    def get_degree_submatrix(self, degree: int) -> pandas.DataFrame:
+        column_names = list(self.columns)
+        column_names = list(filter(lambda x: len(x) == degree, column_names))
+        if (degree == 0):
+            column_names = [(-1,)]
+        elif (degree == 1):
+            column_names.pop()
+        return self.loc[:, self.columns.isin(column_names)]
 
 
-    # def get_linear_submatrix(self) -> pandas.DataFrame:
-    #     linear = pandas.concat([self.get_degree_submatrix(1), self.get_degree_submatrix(0)], axis=1)
-    #     rest = self[~self.isin(linear)].dropna(axis=1)
-    #     rows = []
-    #     for key, row in rest.iterrows():
-    #         if row.sum() == 0:
-    #             rows.append(key)
-    #     return linear.T.loc[:, linear.T.columns.isin(rows)].T # can be probably done better
+    def get_linear_submatrix(self) -> pandas.DataFrame:
+        linear = pandas.concat([self.get_degree_submatrix(1), self.get_degree_submatrix(0)], axis=1)
+        rest = self[~self.isin(linear)].dropna(axis=1)
+        rows = []
+        for key, row in rest.iterrows():
+            if row.sum() == 0:
+                rows.append(key)
+        return linear.T.loc[:, linear.T.columns.isin(rows)].T # can be probably done better
 
 
-    # def numpy_linalg_solve(self) -> pandas.DataFrame:
-    #     coeff_matrix = pandas.DataFrame.to_numpy(self.iloc[:, 0:len(self)], dtype=np.uint8)
-    #     ordinate_values = pandas.DataFrame.to_numpy(self.iloc[:, len(self):len(self)+1], dtype=np.uint8)
-    #     result = np.linalg.solve(coeff_matrix, ordinate_values).astype(np.int8).T
-    #     columns = self.columns[:-1]
-    #     if result.shape[1] != len(self.columns)-1:
-    #         return pandas.DataFrame([])
-    #     return pandas.DataFrame(result, columns=columns)
+    def numpy_linalg_solve(self) -> pandas.DataFrame:
+        if (self.iloc[:, :-1] == 0).all(axis=1).any():
+            return pandas.DataFrame([])
+        coeff_matrix = pandas.DataFrame.to_numpy(self.iloc[:, 0:len(self)], dtype=np.uint8)
+        ordinate_values = pandas.DataFrame.to_numpy(self.iloc[:, len(self):len(self)+1], dtype=np.uint8)
+        result = np.linalg.solve(coeff_matrix, ordinate_values).astype(np.int8).T
+        columns = self.columns[:-1]
+        if result.shape[1] != len(self.columns)-1:
+            return pandas.DataFrame([])
+        return pandas.DataFrame(result, columns=columns)
 
 
-    # def set_variable(self, variable: int, value: int, guessed: bool = False, drop: bool = True)-> None:
-    #     self.solution[variable] = value
-    #     if guessed:
-    #         if (not variable in self.guessed_variables):
-    #             self.guessed_variables.append(variable)
-    #     columns = sorted(self.columns, key=len, reverse=True)
-    #     for column in columns:
-    #         if variable in column:
-    #             if value == 1:
-    #                 new_column = list(column)
-    #                 new_column.remove(variable)
-    #                 if len(new_column) == 0:
-    #                     new_column = (-1,)
-    #                 else:
-    #                     new_column = tuple(new_column)
-    #                 self[new_column] ^= self[column]
-    #             if (drop):
-    #                 self.drop(column, axis=1, inplace=True)
-    #             else:
-    #                 self[column] = 0
-
-
-    # def set_x_variables_to_zero(self, x: int, drop: bool = True) -> None:
-    #     variables = [x for x in range(1, var_count+1)]
-    #     variables = np.random.permutation(variables)
-    #     variables = variables[:x]
-    #     for variable in variables:
-    #         self.set_variable(variable, 0, True, drop)
-
-
-    # def is_in_rref(self) -> bool:
-    #     zero_row = False
-    #     prev_index = -1
-    #     for i in range(len(self)):
-    #         row = self.iloc[i]
-    #         row_ones = row[row > 0]
-    #         if row_ones.sum() == 0:
-    #             zero_row = True
-    #             continue
-    #         elif row_ones.sum() > 0 and zero_row:
-    #             return False
-    #         index = row_ones.index[0]
-    #         if self[index].sum() > 1:
-    #             return False
-    #         if self.columns.get_loc(index) <= prev_index:
-    #             return False
-    #         prev_index = self.columns.get_loc(index)
-    #     return True
-
-
-    # @numba.jit(parallel=True, fastmath=True)
-    # def generate_rows(self, prev_row_count: int, degree_range: range):
-    #     new_index = prev_row_count
-    #     for i in numba.prange(var_count):
-    #         if (i+1 in self.solution.keys()):
-    #             continue
-    #         for j in degree_range:
-    #             if (self.generate_row(i+1, j, new_index)):
-    #                 new_index += 1
-
-
-    # @numba.jit(parallel=True, fastmath=True)
-    # def generate_row(self, variable: int, row_index: int, row_insert: int) -> bool:
-    #     new_row = self.iloc[row_index].copy()
-    #     for column_name, value in new_row.items(): #if columns are swapped it may not work
-    #         if (value == 1):
-    #             if (variable in column_name):
-    #                 continue
-    #             if (len(column_name) == 1 and column_name[0] == -1):
-    #                 new_index = (variable,)
-    #             else:
-    #                 new_index = tuple(sorted(list((variable,) + column_name)))
-    #             if (len(new_index) > degree):
-    #                 return False
-    #             new_row[new_index] = new_row[new_index] ^ 1
-    #             new_row[column_name] = 0
-    #     self.iloc[row_insert] = new_row
-    #     return True
-
-
-    # # @numba.jit
-    # def test(self) -> None:
-    #     global degree
-        
-    #     start_time = time.time()
-    #     finished: bool = False
-
-    #     self = GF2Mat(self._xor_same_indices())
-    #     copy = GF2Mat(self.copy())
-    #     self.set_x_variables_to_zero(var_count//2)
-    #     guessed = self.guessed_variables.copy()
-    #     # total = pow(2, var_count//2)
-    #     # comb = 0
-    #     degree_rows = {}
-    #     for combination in tqdm(self._get_next_combination(var_count//2), total=pow(2, var_count//2)):
-    #         if finished:
-    #             break
-    #         # comb += 1
-    #         # print(f"{comb} out of {total}, {combination}")
-    #         self = GF2Mat(copy.copy())
-    #         for i in range(len(guessed)):
-    #             self.set_variable(guessed[i], combination[i], True)
-    #         degree_rows[2] = range(0, self.shape[0])
-    #         # degree_rows = [0] # should probably contain ranges, doesn't work after galois_row_reduce()
-    #         # degree_rows.append(self.shape[0])
-    #         while True:
-    #             degree += 1
-    #             # degree_rows.append(self.shape[0] - sum(degree_rows))
-    #             new_col_names = generate_all_column_names(degree, var_count, guessed, True)
-    #             prev_row_count = (self.get_degree_last_row(degree-1) + 1) - self.get_degree_first_row(degree-1)
-    #             total_row_count =  ((var_count - len(guessed)) + 1) * prev_row_count
-    #             self = GF2Mat(self.reindex(columns=new_col_names, index=range(total_row_count), fill_value=0), dtype=np.int0)
-    #             self.generate_rows(prev_row_count, degree_rows[degree-1])
-    #             self = GF2Mat(self.galois_row_reduce())
-    #             self = GF2Mat(self.drop_empty_rows())
-    #             self = GF2Mat(self.drop_zeros_only_columns())
-    #             for deg in range(2, degree+1):
-    #                 degree_rows[deg] = self.get_degree_range(deg)
-    #             if (self.shape[0] < self.shape[1]-1):
-    #                 continue
-    #             if (self.has_solution()):
-    #                 linear = GF2Mat(self.get_linear_submatrix())
-    #                 result = linear.numpy_linalg_solve()
-    #                 if (len(result)):
-    #                     test_solution = []
-    #                     dict_result = result.to_dict()
-    #                     for x in range(1, var_count+1):
-    #                         if x in self.solution.keys():
-    #                             test_solution.append(self.solution[x])
-    #                         else:
-    #                             test_solution.append(dict_result[(x,)][0])
-    #                     if (copy.is_valid_solution(test_solution)):
-    #                         finished = True
-    #                         print(result)
-    #                         print(self.solution)
-    #             degree = 2
-    #             break
-                
-    #     end_time = time.time()
-    #     elapsed_time = end_time - start_time
-    #     print(elapsed_time)
-
-
-    # def get_degree_range(self, degree: int) -> range:
-    #     min = self.get_degree_first_row(degree)
-    #     max = self.get_degree_last_row(degree)
-    #     return range(min, max+1)
-    
-
-    # def get_degree_first_row(self, degree: int) -> int:
-    #     min_index = 0
-    #     max_index = self.shape[0] - 1
-    #     index = (min_index + max_index) // 2
-
-    #     if self.get_row_degree(min_index) == degree:
-    #         return min_index
-
-    #     while min_index <= max_index:
-    #         index = (min_index + max_index) // 2
-    #         if self.get_row_degree(index-1) != degree and self.get_row_degree(index) == degree:
-    #             return index
-    #         if self.get_row_degree(index) <= degree:
-    #             max_index = index - 1
-    #         else:
-    #             min_index = index + 1
-
-    #     return -1
-
-
-
-    # def get_degree_last_row(self, degree: int) -> int:
-    #     min_index = 0
-    #     max_index = self.shape[0] - 1
-
-    #     if self.get_row_degree(max_index) == degree:
-    #         return max_index
-
-    #     while min_index <= max_index:
-    #         index = (min_index + max_index) // 2
-    #         if self.get_row_degree(index+1) != degree and self.get_row_degree(index) == degree:
-    #             return index
-    #         if self.get_row_degree(index) < degree:
-    #             max_index = index - 1
-    #         else:
-    #             min_index = index + 1
-
-    #     return -1
-
-
-    # def row_starts_with_degree(self, degree: int, row_index: int) -> bool:
-    #     row = self.iloc[row_index]
-    #     for col, value in row.items():
-    #         if len(col) < degree:
-    #             break
-    #         if value == 1:
-    #             if (len(col) == degree):
-    #                 return True
-    #             return False
-    #     return False
-
-
-    # def get_row_degree(self, row_index: int) -> int:
-    #     row = self.iloc[row_index]
-    #     for col, value in row.items():
-    #         if value == 1:
-    #             return len(col)
-    #     return -1
-
-
-
-    # def _get_next_combination(self, var_count: int):
-    #     solutions = itertools.product([0, 1], repeat=var_count)
-    #     for solution in solutions:
-    #         yield solution
+    def set_variable(self, variable: int, value: bool, drop: bool = True)-> None:
+        columns = sorted(self.columns, key=len, reverse=True)
+        for column in columns:
+            if variable in column:
+                if value:
+                    new_column = list(column)
+                    new_column.remove(variable)
+                    if len(new_column) == 0:
+                        new_column = (-1,)
+                    else:
+                        new_column = tuple(new_column)
+                    self[new_column] ^= self[column]
+                if (drop):
+                    self.drop(column, axis=1, inplace=True)
+                else:
+                    self[column] = 0
 
 
     def transform_to_z3(self, row_indices: list[int], ctx: Context) -> Solver:
@@ -361,11 +165,9 @@ class GF2Mat(pandas.DataFrame):
         return solver
 
     # add threadpools for init?
-    def init_solvers(self, group_size) -> tuple[list[Solver], dict[Solver, Context]]:
-        row_count = self.shape[0]
+    def init_solvers(self, group_size: int, solver_count: int) -> tuple[list[Solver], dict[Solver, Context]]:
         solvers = []
         contexts: dict[Solver, Context] = {}
-        solver_count = np.int32(np.ceil(row_count/group_size) * 1.5)
         
         for _ in range(solver_count):
             rows = list(self.sample(n=group_size).T.columns)
@@ -400,8 +202,8 @@ class GF2Mat(pandas.DataFrame):
             arr = reversed_stats[key]
             for i in range(len(arr)-1):
                 assumptions.append(Bool(arr[i]) == Bool(arr[i+1]))
-                
-        
+
+
         for key in stats.keys():
             if stats[key] == 0 and not (key in determined_vars):
                 determined_vars[key] = False
@@ -429,6 +231,25 @@ class GF2Mat(pandas.DataFrame):
                 if stats[key] <= min_stat+threshold and not (key in determined_vars):
                     determined_vars[key] = False
                     assumptions.append(Not(Bool(key)))
+    
+    
+    def solve_linear(self, determined_vars: dict[str, bool]) -> pandas.DataFrame:
+        matrix = GF2Mat(self.copy())
+        for key, value in determined_vars.items():
+            var = int(re.search(r'\d+', key).group())
+            matrix.set_variable(var, value)
+        linear = GF2Mat(matrix.get_linear_submatrix())
+        linear = GF2Mat(linear.galois_row_reduce())
+        linear = GF2Mat(linear.drop_empty_rows())
+        if linear.shape[0] < linear.shape[1] - 1:
+            return pandas.DataFrame()
+        if linear.shape[0] == linear.shape[1] - 1:
+            return linear.numpy_linalg_solve()
+        
+        linear = GF2Mat(linear[:-(linear.shape[0] - (linear.shape[1]-1))])
+        return linear.numpy_linalg_solve()
+        
+        
 
 
     def solve_combination(self, combination: tuple[bool, ...], solvers: list[Solver], contexts: dict[Solver, Context], offset: int=0) -> list[int]:
@@ -437,8 +258,10 @@ class GF2Mat(pandas.DataFrame):
         stats: dict[str, int] = {}
         self.set_vars(combination, assumptions, determined_vars, offset)
 
-        while len(determined_vars) < var_count:
-            self.determine_additional_vars(stats, assumptions, determined_vars)
+        vars_needed = self.vars_needed
+
+        while len(determined_vars) < vars_needed:
+            self.determine_additional_vars(stats, assumptions, determined_vars, var_count*2)
             
             assumptions_ctx = {}
             for solver in solvers:
@@ -469,15 +292,30 @@ class GF2Mat(pandas.DataFrame):
                     return result
                 for key in stat.keys():
                     stats[key] += stat[key]
+        
+        res = self.solve_linear(determined_vars)
+        
+        if len(res):
+            for key, value in res.to_dict().items():
+                determined_vars[f"x{key[0]}"] = value[0] == 1
+
+            result = []
+            for i in range(1, var_count+1):
+                result.append(1 if f"x{i}" in determined_vars and determined_vars[f"x{i}"] else 0)
+            is_result = self.is_valid_solution(result)
+            if is_result:
+                print(result)
+                return result
         return []
 
 
-    def find_solution(self, group_size: int):
-        solvers, contexts = self.init_solvers(group_size)
+    def find_solution(self, group_size: int = 10, solver_count: int = 10, start_len: int = 1):
+        solvers, contexts = self.init_solvers(group_size, solver_count)
                 
         self.set_most_common()
+        self.set_vars_needed()
 
-        for i in range(1, var_count+1):
+        for i in range(start_len, var_count+1):
             combinations = itertools.product([False, True], repeat=i)
             for combination in tqdm(combinations, total=pow(2, i)):
                 res = self.solve_combination(combination, solvers, contexts)
@@ -500,7 +338,7 @@ class GF2Mat(pandas.DataFrame):
             
         factor = self.num_of_satisfied_equations(result)
         
-        if factor >= (var_count * 2) - 1:
+        if factor >= self.shape[0] - 1:
             return stats, True
         
         modified_stats = {key: value * factor for key, value in stats.items()}
@@ -517,7 +355,24 @@ class GF2Mat(pandas.DataFrame):
             for var in column:
                 most_common[f'x{var}'] += col_sum
 
-        self.most_common = dict(sorted(most_common.items(), key=lambda item: item[1], reverse=True)) 
+        self.most_common = dict(sorted(most_common.items(), key=lambda item: item[1], reverse=True))
+
+
+    def set_vars_needed(self) -> None:
+        row_count = self.shape[0]
+        col_count = self.shape[1]
+        x = Int('x')
+
+        opt = Optimize()
+
+        opt.add(x <= var_count, x >= 0)
+        opt.add(col_count - (((var_count - x + 1) * (var_count + x)) / 2) <= row_count - x)
+
+        opt.maximize(x)
+
+        opt.check()
+
+        self.vars_needed = var_count - opt.model()[x].as_long()
 
 
 def find_keys_with_same_values(my_dict: dict) -> dict:
@@ -596,8 +451,8 @@ def load_data_from_file(path: str) -> tuple[int, list]:
 if (__name__ == '__main__'):
     np.random.seed(0)
 
-    # solution_key = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, ] # 30
-    solution_key = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, ] # 20
+    solution_key = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, ] # 30
+    # solution_key = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, ] # 20
     # solution_key = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, ] # 15
     # solution_key = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, ] # 10
     # solution_key = [0, 1, 1, 0, 1, ] # 5
@@ -612,7 +467,7 @@ if (__name__ == '__main__'):
     matrix = GF2Mat(matrix._xor_same_indices())
     matrix = GF2Mat(matrix.galois_row_reduce())
 
-    cProfile.run("matrix.find_solution(10)", "test_profile")
+    cProfile.run("matrix.find_solution(10, 10, 10)", "test_profile")
 
     # cProfile.run("matrix.test()", "test_profile")
     stats = pstats.Stats("test_profile")
